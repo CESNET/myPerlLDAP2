dcd9a3125441ddc8ed91e17a9b1fd62c0031caa4
semik <semik>
2001-06-01 10:21:11 +0000

    Initial commit

---
+++ myPerlLDAP/conn.pm
@@ -0,0 +1,427 @@
+package myPerlLDAP::Conn;
+
+use strict;
+use Mozilla::OpenLDAP::Conn;
+# TODO: Dont' import all ... mod_perl eff :(
+use Mozilla::OpenLDAP::API 1.4 qw(/.+/);
+use Mozilla::OpenLDAP::Utils qw(str2Scope normalizeDN);
+use myPerlLDAP::Entry;
+use myPerlLDAP::SearchResults;
+
+use vars qw($VERSION);
+
+$VERSION = "0.0.1";
+
+#############################################################################
+# Creator, create and initialize a new LDAP object ("connection"). We support
+# either providing all parameters as a hash array, or as individual
+# arguments.
+#
+# Without any changes copied from perLDAP-1.4
+#
+sub new {
+  my ($class, $self) = (shift, {});
+
+  if (ref $_[$[] eq "HASH") {
+    my ($hash);
+
+    $hash = $_[$[];
+    $self->{"host"} = $hash->{"host"} if defined($hash->{"host"});
+    $self->{"port"} = $hash->{"port"} if defined($hash->{"port"});
+    $self->{"binddn"} = $hash->{"bind"} if defined($hash->{"bind"});
+    $self->{"bindpasswd"} = $hash->{"pswd"} if defined($hash->{"pswd"});
+    $self->{"certdb"} = $hash->{"cert"} if defined($hash->{"cert"});
+  } else {
+    my ($host, $port, $binddn, $bindpasswd, $certdb, $authmeth) = @_;
+
+    $self->{"host"} = $host;
+    $self->{"port"} = $port;
+    $self->{"binddn"} = $binddn;
+    $self->{"bindpasswd"} = $bindpasswd;
+    $self->{"certdb"} = $certdb;
+  };
+
+  $self->{"binddn"} = "" unless defined($self->{"binddn"});
+  $self->{"bindpasswd"} = "" unless defined($self->{"bindpasswd"});
+
+  if (!defined($self->{"port"}) || ($self->{"port"} eq "")) {
+    $self->{"port"} = (($self->{"certdb"} ne "") ? LDAPS_PORT : LDAP_PORT);
+  }
+  bless $self, $class;
+
+  return unless $self->init();
+  return $self;
+}; # new -------------------------------------------------------------------
+
+#############################################################################
+# Destructor, makes sure we close any open LDAP connections.
+#
+# Without any changes copied from perLDAP-1.4
+#
+sub DESTROY {
+  my ($self) = shift;
+
+  return unless defined($self->{"ld"});
+
+  $self->close();
+}; # DESTROY ----------------------------------------------------------------
+
+#############################################################################
+# Initialize a normal connection. This seems silly, why not just merge
+# this back into the creator method (new)...
+#
+# Folowing code is based on perlLDAP-1.4 was modified by Milan (I think)
+# for OpenLDAP, and some other minor changes were done by me ... it will
+# not be merged back to constructor I like this way ;-)
+sub init {
+  my ($self) = shift;
+  my ($ret, $ld);
+
+  if (defined($self->{"certdb"}) && ($self->{"certdb"} ne "")) {
+    $ret = ldapssl_client_init($self->{"certdb"}, 0);
+    return if ($ret < 0);
+
+    $ld = ldapssl_init($self->{"host"}, $self->{"port"}, 1);
+  } else {
+    $ld = ldap_init($self->{"host"}, $self->{"port"});
+  };
+  return unless $ld;
+
+  $self->{"ld"} = $ld;
+  $ret = ldap_simple_bind_s($ld, $self->{"binddn"}, $self->{"bindpasswd"});
+
+  return (($ret == LDAP_SUCCESS) ? 1 : undef);
+} # init --------------------------------------------------------------------
+
+#############################################################################
+# Create a new, empty, Entry object.
+#
+# Personaly I don't se any reason for this function, but I'm will leave it
+# here for users migrating from Mozilla:perLDAP or Mozilla:OpenLDAP
+#
+sub newEntry {
+
+  return new myPerlLDAP::Entry;
+} # newEntry ----------------------------------------------------------------
+
+#############################################################################
+# Checks if a string is a properly formed LDAP URL.
+#
+# Without any changes copied from perLDAP-1.4
+#
+sub isURL {
+  my ($self, $url) = @_;
+
+  return ldap_is_ldap_url($url);
+} # isURL -------------------------------------------------------------------
+
+#############################################################################
+# Return the actual low level LD connection structure, which is needed if
+# you want to call any of the API functions yourself...
+#
+# Without any changes copied from perLDAP-1.4
+#
+sub getLD {
+  my ($self) = shift;
+
+  return $self->{"ld"} if defined($self->{"ld"});
+}; # getLD ------------------------------------------------------------------
+
+#############################################################################
+# Return the actual the current result message, don't use this unless you
+# really have to...
+#
+# Without any changes copied from perLDAP-1.4
+#
+sub getRes {
+  my ($self) = shift;
+
+  return $self->{"ldres"} if defined($self->{"ldres"});
+}; # getRes -----------------------------------------------------------------
+
+#############################################################################
+# Return the Error code from the last LDAP api function call. The last two
+# optional arguments are pointers to strings, and will be set to the
+# match string and extra error string if appropriate.
+#
+# Without any change copied from perLDAP-1.4
+#
+sub getErrorCode {
+  my ($self, $match, $msg) = @_;
+  my ($ret);
+
+  return LDAP_SUCCESS unless defined($self->{"ld"});
+  return ldap_get_lderrno($self->{"ld"}, $match, $msg);
+}; # getErrorCode -----------------------------------------------------------
+
+#############################################################################
+# Return the Error string from the last LDAP api function call.
+#
+# Without any change copied from perLDAP-1.4
+#
+sub getErrorString {
+  my ($self) = shift;
+  my ($err);
+
+  return LDAP_SUCCESS unless defined($self->{"ld"});
+  $err = ldap_get_lderrno($self->{"ld"}, undef, undef);
+
+  return ldap_err2string($err);
+} # getErrorString ----------------------------------------------------------
+
+#############################################################################
+# Print the last error code...
+#
+# Without any change copied from perLDAP-1.4
+#
+sub printError {
+  my ($self, $str) = @_;
+
+  return unless defined($self->{"ld"});
+
+  $str = "LDAP error:" unless defined($str);
+  print "$str ", $self->getErrorString(), "\n";
+} # printError --------------------------------------------------------------
+
+#############################################################################
+# Normal LDAP search. Note that this will actually perform LDAP URL searches
+# if the filter string looks like a proper URL.
+#
+# Without any change copied from perLDAP-1.4
+# TODO: CLEAN
+sub search {
+  my ($self, $basedn, $scope, $filter, $attrsonly, @attrs) = @_;
+  my ($resv, $entry);
+  my ($res) = \$resv;
+
+  $scope = str2Scope($scope);
+  $filter = "(objectclass=*)" if ($filter =~ /^ALL$/i);
+
+  if (defined($self->{"ldres"})) {
+    ldap_msgfree($self->{"ldres"});
+    undef $self->{"ldres"};
+  };
+
+  if (ldap_is_ldap_url($filter)) {
+    if (! ldap_url_search_s($self->{"ld"}, $filter, $attrsonly, $res)) {
+      #$self->{"ldres"} = $res;
+      #$self->{"ldfe"} = 1;
+      #$entry = $self->nextEntry();
+      return new myPerlLDAP::SearchResults($self->{ld}, $res);
+    };
+  } else {
+    if (! ldap_search_s($self->{"ld"}, $basedn, $scope, $filter,
+			defined(\@attrs) ? \@attrs : 0,
+			defined($attrsonly) ? $attrsonly : 0,
+			defined($res) ? $res : 0)) {
+      #$self->{"ldres"} = $res;
+      #$self->{"ldfe"} = 1;
+      #$entry = $self->nextEntry();
+      return new myPerlLDAP::SearchResults($self->{ld}, $res);
+    };
+  };
+
+  return $entry;
+} # search ------------------------------------------------------------------
+
+#############################################################################
+# URL search, optimized for LDAP URL searches.
+#
+# Without any change copied from perLDAP-1.4
+#
+sub searchURL {
+  my ($self, $url, $attrsonly) = @_;
+  my ($resv, $entry);
+  my ($res) = \$resv;
+
+  if (defined($self->{"ldres"})) {
+    ldap_msgfree($self->{"ldres"});
+    undef $self->{"ldres"};
+  };
+
+  if (! ldap_url_search_s($self->{"ld"}, $url,
+			  defined($attrsonly) ? $attrsonly : 0,
+			  defined($res) ? $res : 0)) {
+    $self->{"ldres"} = $res;
+    $self->{"ldfe"} = 1;
+    $entry = $self->nextEntry();
+  };
+
+  return $entry;
+}; # searchURL -------------------------------------------------------------
+
+#############################################################################
+# Browse an LDAP entry, very much like the regular search, except we set
+# some defaults (like scope=BASE, filter=(objectclass=*) and so on). Note
+# that this method does not support the attributesOnly flag.
+#
+# Based on perlLDAP-1.4 code (I little reduced code)
+#
+sub browse {
+  my ($self, $basedn, @attrs) = @_;
+
+  return  $self->search($basedn, LDAP_SCOPE_BASE, "(objectclass=*)",
+			0, @attrs);
+}; # browse -----------------------------------------------------------------
+
+#############################################################################
+# Compare an attribute value against a DN in the server (without having to
+# do a search first).
+#
+# Without any change copied from perLDAP-1.4
+#
+sub compare {
+  my ($self, $dn, $attr, $value) = @_;
+
+  return ldap_compare_s($self->{"ld"}, $dn, $attr, $value) ==
+    LDAP_COMPARE_TRUE;
+}; # compare ----------------------------------------------------------------
+
+#############################################################################
+# Close the connection to the LDAP server.
+#
+# With minor change copied from perLDAP-1.4
+#
+sub close {
+  my $self = shift;
+  my $ret = 1;
+
+  ldap_unbind_s($self->{"ld"}) if defined($self->{"ld"});
+  if (defined($self->{"ldres"})) {
+    ldap_msgfree($self->{"ldres"});
+    undef $self->{"ldres"};
+  };
+  undef $self->{"ld"};
+
+  return (($ret == LDAP_SUCCESS) ? 1 : undef);
+}; # close ------------------------------------------------------------------
+
+#############################################################################
+# Delete an object. An Entry ...
+#
+# With minor changes copied from perLDAP-1.4
+#
+# TODO: This will not work with clases based on myLDAP::Entry or will??
+sub delete {
+  my $self = shift;
+  my $id = shift;
+  my $dn = $id;
+
+  if (ref($id) eq 'myLDAP::Entry') {
+    $dn = $id->getDN();
+  } else {
+    $dn = $self->{"dn"} unless (defined($dn) && ($dn ne ""));
+  };
+
+  $dn = normalizeDN($dn);
+  my $ret = ldap_delete_s($self->{"ld"}, $dn) if ($dn ne "");
+
+  return (($ret == LDAP_SUCCESS) ? 1 : undef);
+}; # delete -----------------------------------------------------------------
+
+#############################################################################
+# Add an object. This fuction expect as argument myPerlLDAP::Entry,
+# it will not accept HASH like original function from perlLDAP!
+#
+# Based on perlLDAP, heavy modified for my new Entry ... most of code was
+# deleted, functionality is now in Entry::makeAddRecord
+#
+sub add {
+  my ($self, $entry) = @_;
+
+  return unless $entry;
+
+  my $rec = $entry->makeAddRecord;
+  my $ret = ldap_add_s($self->{"ld"}, $entry->getDN, $rec);
+
+  if ($ret == LDAP_SUCCESS) {
+    $entry->clearModifiedFlags;
+    return 1;
+  } else {
+    return undef;
+  };
+}; # add --------------------------------------------------------------------
+
+#############################################################################
+# Modify the RDN, and update the entry accordingly. Note that the last
+# two arguments (DN and "delete") are optional. The last (optional) argument
+# is a flag, which if set to TRUE (the default), will cause the corresponding
+# attribute value to be removed from the entry.
+#
+# Without any change copied from perLDAP-1.4
+#
+sub modifyRDN {
+  my ($self, $rdn, $dn, $del) = ($_[$[], $_[$[ + 1], $_[$[ + 2], $_[$[ + 3]);
+  my (@vals);
+  my ($ret) = 1;
+
+  $del = 1 unless (defined($del) && ($del ne ""));
+  $dn = $self->{"dn"} unless (defined($dn) && ($dn ne ""));
+
+  @vals = ldap_explode_dn($dn, 0);
+  if (lc($vals[$[]) ne lc($rdn)) {
+    $ret = ldap_modrdn2_s($self->{"ld"}, $dn, $rdn, $del);
+    if ($ret == LDAP_SUCCESS) {
+      shift(@vals);
+      unshift(@vals, ($rdn));
+      $self->{"dn"} = join(@vals);
+    };
+  };
+
+  return (($ret == LDAP_SUCCESS) ? 1 : 0);
+}; # modifyRDN --------------------------------------------------------------
+
+#############################################################################
+# Update an object.
+#
+# Inspired by perlLDAP-1.4 code - most of code was deleted functionality
+# is now in Entry::makeModificationRecord. Each book about OOP says:
+#
+#    "Nerver let user or other class using your class touch it's
+#     internal structures."
+#
+sub update {
+  my ($self, $entry) = @_;
+
+  my $rec = $entry->makeModificationRecord;
+  my $ret = ldap_modify_s($self->{"ld"}, $entry->getDN, $rec);
+
+  if ($ret == LDAP_SUCCESS) {
+    $entry->clearModifiedFlags;
+    return 1;
+  } else {
+    return undef
+  };
+}; # update -----------------------------------------------------------------
+
+#############################################################################
+# Set the rebind procedure. We also provide a neat default rebind procedure,
+# which takes three arguments (DN, password, and the auth method). This is an
+# extension to the LDAP SDK, which I think should be there. It was also
+# needed to get this to work on Win/NT...
+#
+# Without any change copied from perLDAP-1.4 ... I've no idea what is this
+#
+sub setRebindProc {
+  my ($self, $proc) = @_;
+
+  # Should we try to reinitialize the connection?
+  die "No LDAP connection" unless defined($self->{"ld"});
+
+  ldap_set_rebind_proc($self->{"ld"}, $proc);
+}; # setRebindProc ----------------------------------------------------------
+
+sub setDefaultRebindProc {
+  my ($self, $dn, $pswd, $auth) = @_;
+
+  $auth = LDAP_AUTH_SIMPLE unless defined($auth);
+  die "No LDAP connection"
+    unless defined($self->{"ld"});
+
+  ldap_set_default_rebind_proc($self->{"ld"}, $dn, $pswd, $auth);
+} # setDefaultRebindProc ----------------------------------------------------
+
+#############################################################################
+# Mandatory TRUE return value.
+#
+1;
---
+++ myPerlLDAP/searchResults.pm
@@ -0,0 +1,123 @@
+#!/usr/bin/perl -w
+
+package myPerlLDAP::SearchResults;
+
+use Mozilla::OpenLDAP::API 1.4 qw(/.+/);
+use strict;
+use Carp;
+
+use vars qw($VERSION);
+
+$VERSION = "0.0.1";
+
+my $_D = 1;
+
+# TODO:
+#   - firstNetry
+#   - count      ... I acctualy don't need this, but they
+#                    should be fine
+
+sub new {
+  my $proto = shift;
+  my $class = ref($proto) || $proto;
+  my $self = {};
+
+  $self->{_D} = $_D;
+
+  if (($_D >= 10) || ($self->{_D} >= 10)) {
+    carp("$class created");
+  };
+
+  bless($self, $class);
+  return unless $self->init(@_);
+
+  return $self;
+};
+
+sub init {
+  my $self = shift;
+  my $ld = shift;
+  my $res = shift;
+
+  if (defined($ld) and defined($res)) {
+    $self->{ldres}=$res;
+    $self->{ld}=$ld;
+    $self->{ldfe}=1;
+
+    return $self;
+  } else {
+    carp("$self->init requires ld and res args");
+    return undef;
+  };
+};
+
+#############################################################################
+# Get an entry from the search, either the first entry, or the next entry,
+# depending on the call order.
+#
+# Based on perlLDAP-1.4 code, I did heavy modification to be usefull
+# with my new Entry. Originaly it was part of the Conn class, but
+# I don't like way how multiple searches on same ld are done in perlLDAP-1.4
+# so I created this class and moved some code here.
+#
+# TODO: Clean up code ... delete old code!
+sub nextEntry {
+  my $self = shift;
+  my (%entry, @vals);
+  my ($attr, $lcattr, $ldentry, $berv, $dn, $count);
+  my ($ber) = \$berv;
+
+  my $entry = new myPerlLDAP::Entry;
+
+  if ($self->{"ldfe"} == 1) {
+    return unless defined($self->{"ldres"});
+
+    $self->{"ldfe"} = 0;
+    $ldentry = ldap_first_entry($self->{"ld"}, $self->{"ldres"});
+    $self->{"ldentry"} = $ldentry;
+  } else {
+    return unless defined($self->{"ldentry"});
+
+    $ldentry = ldap_next_entry($self->{"ld"}, $self->{"ldentry"});
+    $self->{"ldentry"} = $ldentry;
+  };
+
+  if (! $ldentry) {
+    if (defined($self->{"ldres"})) {
+      ldap_msgfree($self->{"ldres"});
+      undef $self->{"ldres"};
+    }
+    return "";
+  };
+
+  $dn = ldap_get_dn($self->{"ld"}, $self->{"ldentry"});
+  $entry->setDN($dn);
+
+  $attr = ldap_first_attribute($self->{"ld"}, $self->{"ldentry"}, $ber);
+  $entry->clearModifiedFlags;
+  return $entry unless $attr;
+
+  $lcattr = lc $attr;
+  @vals = ldap_get_values_len($self->{"ld"}, $self->{"ldentry"}, $attr);
+  if ($lcattr =~ /^[^;]+$/) {
+    $entry->addAsValues($lcattr, @vals);
+  };
+
+  $count = 1;
+  while ($attr = ldap_next_attribute($self->{"ld"},
+				     $self->{"ldentry"}, $ber)) {
+    $lcattr = lc $attr;
+    @vals = ldap_get_values_len($self->{"ld"}, $self->{"ldentry"}, $attr);
+    # TODO: just for now ... I want to work with types ...
+    if ($lcattr =~ /^[^;]+$/) {
+      $entry->addAsValues($lcattr, \@vals);
+    };
+    $count++;
+  };
+
+  ldap_ber_free($ber, 0) if $ber;
+
+  $entry->clearModifiedFlags;
+  return $entry;
+};
+
