8f35ccc64a0155a62ad3104b173cfa32a7a7c732
Jan Tomasek <jan@tomasek.cz>
2001-06-11 18:25:06 +0000

    Added new constants for new tests

--- t/C.pm
+++ t/C.pm
@@ -3,11 +3,14 @@ package C;
 
 use strict;
 use Mozilla::OpenLDAP::API qw(LDAP_PORT LDAPS_PORT LDAP_SCOPE_SUBTREE);
-use vars qw ($LDAPServerHost $LDAPServerPort $SearchFilter $TestBase);
+use vars qw ($LDAPServerHost $LDAPServerPort $SearchFilter $TestBase $BindDN $BindPasswd $TestRDN);
 
 $LDAPServerHost = 'localhost';
 $LDAPServerPort = LDAP_PORT;
-$TestBase       = 'ou=People, o=test';
+$TestBase       = 'ou=People,o=test';
 $SearchFilter   = '(uid=*)';
+$TestRDN        = 'uid=test';
+$BindDN         = 'cn=Manager, o=test';
+$BindPasswd     = 'test_secret';
 
 1;
