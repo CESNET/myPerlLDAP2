4e1c188211d973d787b3333ad70d5ad644d592ab
semik <semik>
2002-04-08 11:20:56 +0000

    Added license and $Id$

--- myPerlLDAP/attribute/_telephoneNumber.pm
+++ myPerlLDAP/attribute/_telephoneNumber.pm
@@ -1,12 +1,31 @@
+#!/usr/bin/perl -w
+#$Id$
+
+# #############################################################################
+# myPerlLDAP - object oriented interface for work with LDAP
+# Copyright (C) 2001,02 by Jan Tomasek
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public
+# License along with this library; if not, write to the Free
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# #############################################################################
+
 package myPerlLDAP::attribute::_telephoneNumber;
 
 use strict;
 use myPerlLDAP::attribute;
 
-use vars qw($VERSION @ISA);
-
-
-$VERSION = "0.5.0";
+use vars qw(@ISA);
 
 @ISA = ("myPerlLDAP::attribute");
 
