0c499c78a05038a8822f3ab3688e7099bf532208
Jan Tomasek <jan@tomasek.cz>
2002-04-08 10:31:20 +0000

    Added license and $Id$

--- myPerlLDAP/searchResults.pm
+++ myPerlLDAP/searchResults.pm
@@ -1,14 +1,35 @@
 #!/usr/bin/perl -w
+#$Id$
+
+# #############################################################################
+# myPerlLDAP - object oriented interface for work with LDAP
+# Copyright (C) 2001,02 by Jan Tomasek
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public
+# License along with this library; if not, write to the Free
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# #############################################################################
 
 package myPerlLDAP::searchResults;
 
-use perlOpenLDAP::API 1.4 qw(ldap_first_entry ldap_next_entry ldap_msgfree ldap_get_dn ldap_first_attribute ldap_next_attribute ldap_get_values_len ldap_ber_free ldap_count_entries);
+use perlOpenLDAP::API 1.4 qw(ldap_first_entry ldap_next_entry ldap_msgfree
+			     ldap_get_dn ldap_first_attribute
+			     ldap_next_attribute ldap_get_values_len
+			     ldap_ber_free ldap_count_entries);
 use strict;
 use Carp;
 
-use vars qw($VERSION $_D);
-
-$VERSION = "0.2.2";
+use vars qw($_D);
 
 $_D = 1;
 
