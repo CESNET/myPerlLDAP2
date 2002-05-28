9d8aaa5f712672664c43cb5c92fc33bde977d1ed
Jan Tomasek <jan@tomasek.cz>
2002-05-28 15:41:54 +0000

    Work on more consistent API ... it break compatibility and classes are right now inoperable.

--- myPerlLDAP/abstract.pm
+++ myPerlLDAP/abstract.pm
@@ -21,10 +21,63 @@
 # #############################################################################
 
 # This class contain abstract class myPerlLDAP::abstract, it implements
-# getErrorCode and getErrorMessage for all other classes in myPerlLDAP
-# module.
+# error and errorMessage for all other classes in myPerlLDAP module.
 
 package myPerlLDAP::abstract;
 
 use strict;
+use Carp;
+use vars qw($_D $AUTOLOAD);
+use perlOpenLDAP::API 1.4 qw(ldap_err2string
+			     LDAP_SUCCESS);
 
+$_D = 1;
+
+my %fields = (
+	      debug       => $_D,
+	      error       => LDAP_SUCCESS,
+	     );
+
+sub new {
+  my $proto = shift;
+  my $class = ref($proto) || $proto;
+  my %FIELDS = %fields;
+
+  my $self = bless {_permitted_fields => \%FIELDS, %fields}, $class;
+
+  if ($_D >= 10) {
+    carp("$class created");
+  };
+
+  return $self;
+};
+
+sub errorMessage {
+  my $self = shift;
+
+  return ldap_err2string($self->error);
+};
+
+sub AUTOLOAD {
+  my $self = shift;
+  my $class = ref($self)
+    or carp "cannot call method $AUTOLOAD on non reference $self";
+  my $name = $AUTOLOAD;
+  $name =~ s/.*://;
+
+  if (exists $self->{_permitted_fields}->{$name}) {
+    if (@_) {
+      return $self->{$name} = shift;
+    } else {
+      return $self->{$name};
+    };
+  } elsif ($name eq 'DESTROY') {
+    if ($self->can('DESTROY')) {
+      $self->DESTROY;
+    } else {
+      return;
+    };
+  } else {
+    carp "Can't access method '$name' in class $class";
+  };
+};
