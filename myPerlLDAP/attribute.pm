3adaab7691ad152ad148b35d37f29c2c08986100
semik <semik>
2002-05-29 16:36:27 +0000

    See debian/changelog version 1.50 for info

--- myPerlLDAP/attribute.pm
+++ myPerlLDAP/attribute.pm
@@ -25,30 +25,32 @@ package myPerlLDAP::attribute;
 use strict;
 
 use Carp;
-use perlOpenLDAP::API qw(LDAP_SUCCESS);
-use vars qw($_D $AUTOLOAD %fields);
+use perlOpenLDAP::API qw(LDAP_SUCCESS LDAP_CONSTRAINT_VIOLATION
+			 LDAP_TYPE_OR_VALUE_EXISTS LDAP_NO_SUCH_OBJECT);
+use myPerlLDAP::attribute;
+use vars qw($AUTOLOAD @ISA %fields);
+
+@ISA = ("myPerlLDAP::abstract");
 
 # Debug levels:
 #  1 ... warnings about nasty class usage
 # 10 ... excution of some methods
-$_D = 1;
 
 %fields = (
-           description   => undef,
-           OID           => undef,
-           equality      => undef,
-           syntax        => undef,
-           subStr        => undef,
-           ordering      => undef,
-           usage         => undef,
-           length        => undef,
-           singleValue   => 0,     # By default multiple values
-           readOnly      => 0,     # By default modifyable?
-           debug         => $_D,
-           modified      => undef,
-           name          => undef,
-	   owner         => undef,
-	   error         => LDAP_SUCCESS, #DELETE
+	   description   => undef,
+	   OID           => undef,
+	   equality      => undef,
+	   syntax        => undef,
+	   subStr        => undef,
+	   ordering      => undef,
+	   usage         => undef,
+	   length        => undef,
+	   singleValue   => 0,     # By default multiple values
+	   readOnly      => 0,     # By default modifyable?
+	   debug         => 1,
+	   modified      => undef,
+	   name          => undef,
+	   owner         => undef
 	  );
 
 =head1 NAME
@@ -69,12 +71,16 @@ sub new {
   my $requestedClass = lc shift;
   my %FIELDS = %fields;
 
-  my $self = bless {_permitted_fields => \%FIELDS, %fields}, $class;
+  my $self = bless $class->SUPER::new(@_), $class;
+  foreach my $element (keys %fields) {
+    $self->{_permitted_fields}->{$element} = $fields{$element};
+  };
+  @{$self}{keys %fields} = values %fields;
 
   $self->name($requestedClass);
   $self->init;
 
-  if (($_D >= 10) || ($self->debug >= 10)) {
+  if ($self->debug >= 10) {
     carp("$self created");
   };
 
@@ -92,7 +98,7 @@ bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla 
 sub init {
   my $self = shift;
 
-  if (($_D >= 10) || ($self->debug >= 10)) {
+  if ($self->debug >= 10) {
     carp("$self initiated");
   };
 };
@@ -101,180 +107,217 @@ sub init {
 sub DESTROY {
   my $self = shift;
 
-  if (($_D >= 10) || ($self->debug >= 10)) {
+  if ($self->debug >= 10) {
     carp("$self destroyed");
   };
 };
 
-sub AUTOLOAD {
-  my $self = shift;
-  my $class = ref($self)
-    or carp "cannot call method $AUTOLOAD on non reference $self";
-  my $name = $AUTOLOAD;
-  $name =~ s/.*://;
-
-  if (exists $self->{_permitted_fields}->{$name}) {
-    if (@_) {
-      return $self->{$name} = shift;
-    } else {
-      return $self->{$name};
-    };
-  } elsif ($name eq 'DESTROY') {
-    if ($self->can('DESTROY')) {
-      $self->DESTROY;
-    } else {
-      return;
-    };
-  } else {
-    carp "Can't access method '$name' in class $class";
-  };
-};
+#sub AUTOLOAD {
+#  my $self = shift;
+#  my $class = ref($self)
+#    or carp "cannot call method $AUTOLOAD on non reference $self";
+#  my $name = $AUTOLOAD;
+#  $name =~ s/.*://;
+#
+#  if (exists $self->{_permitted_fields}->{$name}) {
+#    if (@_) {
+#      return $self->{$name} = shift;
+#    } else {
+#      return $self->{$name};
+#    };
+#  } elsif ($name eq 'DESTROY') {
+#    if ($self->can('DESTROY')) {
+#      $self->DESTROY;
+#    } else {
+#      return;
+#    };
+#  } else {
+#    carp "Can't access method '$name' in class $class";
+#  };
+#};
 
 sub count {
   my $self = shift;
 
-  if (!defined($self->{VALUES})) {
-    return 0;
-  };
-
+  return 0 if (!defined($self->{VALUES}));
   return scalar @{$self->{VALUES}};
 };
 
-sub set {
+# #############################################################################
+# Functions for work with attribute values
+# #############################################################################
+
+# #############################################################################
+# Removes all values from attribute
+#
+# Input: none
+#
+# Output: none
+
+sub clearValues {
   my $self = shift;
-  # This is fastest way how to code this function ;-)
+
+  $self->error(LDAP_SUCCESS);
+
   $self->{VALUES} = undef;
-  return $self->add(@_);
+  $self->setModifiedFlag();
 };
 
-# If possible adds new value(s), accepts two args:
-#  1. array ref or scalar - REQUIRED
-#  2. OPTIONAL type of values scalar only
-sub add {
+# #############################################################################
+# Removes all old values and sets new one
+#
+# Input: see add
+#
+# Output: see add
+
+sub setValues {
   my $self = shift;
 
+  # This READ ONLY attribute we can't change it
   if ($self->readOnly) {
-    # This READ ONLY attribute we can't change it
-    if ($_D || $self->debug) {
-      carp("Attempt to change read-only object $self");
-    };
-    return undef;
+    carp("Attempt to change read-only object $self") if ($self->debug);
+    $self->error(LDAP_CONSTRAINT_VIOLATION);
+    return 0;
   };
 
-  if (!(scalar @_)) {
-    # No value suplied
-    if ($_D || $self->debug) {
-      carp("$self\->add() called without any value");
-    };
-    return undef;
-  };
+  $self->clearValues;
+  return $self->addValues(@_);
+}; # setValues ----------------------------------------------------------------
 
+# #############################################################################
+# Adds new value(s) to attribute
+#
+# Input: value or arrayref of values
+#
+# Output: number of added values
+#
+# Error code should be LDAP_CONSTRAINT_VIOLATION:
+#   1) adding to readOnly attribute such is creatorsname
+#   2) adding second value to singleValue attribute
+# or LDAP_TYPE_OR_VALUE_EXISTS if some value already was in attribute.
+
+sub addValues {
+  my $self = shift;
   my (@values); # Here I put values which will be finally added to
                 # internal classes structures.
   my ($values); # This will be array ref of value(s) passed by user
                 # to this method
+  my $addCounter = 0;
   # Examine if first arg is one value (scalar) or multiple values (array ref)
-  if (ref($_[0]) eq "ARRAY") {
-    $values = shift;
-  } else {
-    my @v;
-    $v[0] = shift;
-    $values = \@v;
-  };
+  $values = $_[0];
+  $values = [$_[0]] if (ref($_[0]) ne 'ARRAY');
+  shift;
   # Subtype of values is optional
   my $type = shift;
 
-  if (($self->singleValue) and (scalar @{$self->get($type)})) {
-    # This attribute is only single value and value is set
-    if ($_D || $self->debug) {
-      carp("$self\->add() another value passed to single-value attribute");
-    };
-    return undef;
+  $self->error(LDAP_SUCCESS);
+
+  # This READ ONLY attribute we can't change it
+  if ($self->readOnly) {
+    carp("Attempt to change read-only object $self") if ($self->debug);
+    $self->error(LDAP_CONSTRAINT_VIOLATION);
+    return 0;
+  };
+
+  # This attribute is only single value and value is set
+  if (($self->singleValue) and (scalar @{$self->getValues($type)})) {
+    carp(ref($self)."\->add() another value passed to single-value attribute")
+      if ($self->debug);
+    $self->error(LDAP_CONSTRAINT_VIOLATION);
+    return 0;
   };
 
   foreach my $value (@$values) {
     if ($self->has($value,$type)) {
-      if ($_D || $self->debug) {
-	#carp("$self\->add() same value=\"$value\" passed to attribute");
-      };
-      return 0;
+      #if ($_D || $self->debug) {
+      #  carp("$self\->add() same value=\"$value\" passed to attribute");
+      #};
+      $self->error(LDAP_TYPE_OR_VALUE_EXISTS);
     } else {
-      my @valElem = ($self->checkFixLength($value), $type);
-      push @values, \@valElem;
+      if ($addCounter and $self->singleValue) {
+	carp("more than one value passed to singe-value attribute $self");
+      } else {
+	my @valElem = ($self->checkFixLength($value), $type);
+	push @values, \@valElem;
+	$addCounter++;
+      };
     };
   };
- ENDFOREACH:
 
-  if (($self->singleValue) and (scalar @values)) {
-    # This attribute is only single value so we take only first
-    if (((scalar @$values) > 2) and ($_D || $self->debug)) {
-      carp("more than one value passed to singe-value attribute $self");
-    };
-    # @values = splice(@values, 2);
-    my $val = $values[0];
-    @values = ($val);
+  if ($addCounter) {
+    push @{$self->{VALUES}}, (@values);
+    $self->setModifiedFlag();
   };
 
-  push @{$self->{VALUES}}, (@values);
+  return $addCounter;
+}; # addValues ----------------------------------------------------------------
 
-  $self->setModifiedFlag();
+# #############################################################################
+# Remove value(s) from attribute
+#
+# Input: value or arrayref of values
+#
+# Output: number of removed values
 
-  return $self->{VALUES};
-};
-
-# Removes list of values from atribute, returns
-sub remove {
+sub removeValues {
   my $self = shift;
 
+  $self->error(LDAP_SUCCESS);
+
+  # This READ ONLY attribute we can't change it
   if ($self->readOnly) {
-    # This READ ONLY attribute we can't change it
-    if ($_D || $self->debug) {
-      carp("Attempt to change read-only object $self");
-    };
+    carp("Attempt to change read-only object $self") if ($self->debug);
+    $self->error(LDAP_CONSTRAINT_VIOLATION);
     return 0;
   };
 
-  my ($v1, $v2, $values);
   # Examine if first arg is one value (scalar) or multiple values (array ref)
-  if (ref($_[0]) eq "ARRAY") {
-    $values = shift;
-  } else {
-    my @v;
-    $v[0] = shift;
-    $values = \@v;
-  };
-  my $t1 = shift;
+  my $values = $_[0];
+  $values = [$_[0]] if (ref($_[0]) ne 'ARRAY');
+  shift;
+  # Subtype of values is optional
+  my $type = shift;
 
-  my $count = 0;
-  if (defined($values)) {
-    foreach $v1 (@$values) {
-      my $j = 0;
-      while ($j < @{$self->{VALUES}}) {
-	$v2 = $self->{VALUES}->[$j];
-	my @v1 = ($v1,$t1);
-        if ($self->compareValues(\@v1, $v2)) {
-	  splice (@{$self->{VALUES}}, $j, 1);
-	  $count++;
-	} else {
-	  $j++;
-	};
+
+  my $v2;
+  my $removedCount = 0;
+
+  foreach my $value (@$values) {
+    my $j = 0;
+    while ($j < @{$self->{VALUES}}) {
+      $v2 = $self->{VALUES}->[$j];
+      my @value = ($value,$type);
+      if ($self->compareValues(\@value, $v2)) {
+	splice (@{$self->{VALUES}}, $j, 1);
+	$removedCount++;
+      } else {
+	$j++;
       };
     };
-  } else {
-    $self->{VALUES}=undef;
   };
 
-  $self->setModifiedFlag() if ($count);
+  $self->setModifiedFlag() if ($removedCount);
+  $self->error(LDAP_NO_SUCH_OBJECT) if ($removedCount != @$values);
 
-  if ($count != (scalar @$values)) {
-    if ($_D || $self->debug) {
-      my $COUNT = scalar @$values;
-      carp("$self\->remove-d only $count of expectected $COUNT values");
+  return $removedCount;
+}; # removeValues -------------------------------------------------------------
+
+sub getValues {
+  my $self = shift;
+  my $type = shift; $type = "" unless $type;
+  my @values;
+
+  foreach my $value (@{$self->{VALUES}}) {
+    my ($v,$t) = @$value;
+    $t = "" unless $t;
+    if ($t eq $type) {
+      push @values, ($v);
     };
   };
 
-  return $count;
-}; # remove -------------------------------------------------------------------
+  return \@values;
+}; # getValues ----------------------------------------------------------------
+
 
 sub has {
   my $self = shift;
@@ -301,7 +344,7 @@ sub has {
       };
     };
   } else {
-    if ($_D || $self->debug) {
+    if ($self->debug) {
       carp("$self\->has() called without any value");
     };
     return undef;
@@ -309,22 +352,6 @@ sub has {
 
   return $count;
 }; # has
-
-sub get {
-  my $self = shift;
-  my $type = shift; $type = "" unless $type;
-  my @values;
-
-  foreach my $value (@{$self->{VALUES}}) {
-    my ($v,$t) = @$value;
-    $t = "" unless $t;
-    if ($t eq $type) {
-      push @values, ($v);
-    };
-  };
-
-  return \@values;
-}; # get
 
 sub types {
   my $self = shift;
@@ -376,7 +403,7 @@ sub checkFixLength {
 
   if (defined($self->length)) {
     if (length($value)>$self->length) {
-      if ($_D || $self->debug) {
+      if ($self->debug) {
 	carp("$self\->checkFixLength() truncated value to $self->{LENGTH} length");
       };
       return substr($value, 0, $self->length);
@@ -414,7 +441,7 @@ sub makeModificationRecord {
     $TYPE = ";$type" if ($type);
     if ($res{$self->name."$TYPE"}->{$mode}) {
     } else {
-      $res{$self->name."$TYPE"}->{$mode} = $self->get($type);
+      $res{$self->name."$TYPE"}->{$mode} = $self->getValues($type);
     };
   };
 
@@ -449,7 +476,7 @@ sub XML {
 
   if ($self->name eq 'objectclass') {
     push @ret, '<dsml:objectclass>';
-    foreach $value (@{$self->get}) {
+    foreach $value (@{$self->getValues}) {
       push @ret, "  <dsml:oc-value>$value</dsml:oc-value>";
     };
     push @ret, '</dsml:objectclass>';
@@ -458,7 +485,7 @@ sub XML {
     $attrArgs = $self->xmlAttributeArgs if ($self->xmlAttributeArgs);
     push @ret, "<dsml:attr name=\"".$self->name."\"$attrArgs>";
     foreach my $type (@{$self->types}) {
-      foreach $value (@{$self->get($type)}) {
+      foreach $value (@{$self->getValues($type)}) {
 	my $TYPE = "";
 	$TYPE = " type=\"$type\"" if ($type);
 
@@ -484,7 +511,7 @@ sub LDIF {
   my $value;
 
   foreach my $type (@{$self->types}) {
-    foreach $value (@{$self->get($type)}) {
+    foreach $value (@{$self->getValues($type)}) {
       my $TYPE = "";
       $TYPE = ";$type" if ($type);
       push @ret, $self->name."$TYPE: $value";
